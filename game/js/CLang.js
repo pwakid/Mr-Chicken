8a6b03ecac60463645f00b3ac7ac06ff89245e13
Travis McKinkley <163606441+pwakid@users.noreply.github.com>
2024-08-06 13:47:03 +0000

    init upload

---
+++ game/js/CLang.js
@@ -0,0 +1,24 @@
+var TEXT_MONEY      = "MONEY";
+var TEXT_PLAY       = "PLAY";
+var TEXT_BET        = "BET";
+var TEXT_COIN       = "COIN";
+var TEXT_MAX_BET    = "MAX BET";
+var TEXT_INFO       = "INFO";
+var TEXT_LINES      = "LINES";
+var TEXT_SPIN       = "SPIN";
+var TEXT_WIN        = "WIN";
+var TEXT_HOLD       = "HOLD";
+var TEXT_HELP_WILD  = "JOLLY SYMBOL CAN REPLACE ANY OTHER SYMBOL TO MAKE UP A COMBO";
+var TEXT_HELP_BONUS = "3 OR MORE MR CHICKEN LET YOU PLAY THE BONUS GAME!";
+var TEXT_CREDITS_DEVELOPED = "DEVELOPED BY";
+var TEXT_CURRENCY = "$";
+var TEXT_PRELOADER_CONTINUE = "START";
+var TEXT_NO_MONEY = "NO MONEY! DO YOU WANT TO RECHARGE?";
+var TEXT_RECHARGE = "RECHARGE";
+var TEXT_EXIT = "EXIT";
+
+var TEXT_CONGRATULATIONS = "Congratulations!";
+var TEXT_MSG_SHARE1 = "You collected <strong>";
+var TEXT_MSG_SHARE2= " points</strong>!<br><br>Share your score with your friends!";
+var TEXT_MSG_SHARING1 = "My score is ";
+var TEXT_MSG_SHARING2 = " points! Can you do better?";
